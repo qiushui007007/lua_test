87c4f60664f8126fabc3abf30e315956b1923be0
qiushui007007 <xxg6688@163.com>
2014-07-23 23:34:41 -0400

    commit_2

--- json_test.lua
+++ json_test.lua
@@ -2,7 +2,7 @@
 JSON4Lua example script.
 Demonstrates the simple functionality of the json module.
 ]]--
-
+-- json.lua在本目录下
 json = require('json')
 
 -- Object to JSON encode
@@ -11,8 +11,8 @@ test = {
 }
 
 jsonTest = json.encode(test)
-
 print('JSON encoded test is: ' .. jsonTest)
+-- JSON encoded test is: {"one":"first","three":[2,3,5],"two":"second"}
 
 -- Now JSON decode the json string
 result = json.decode(jsonTest)
@@ -22,7 +22,7 @@ table.foreach(result,print)
 print ("The decoded table result.three")
 table.foreach(result.three, print)
 
---
+-- 测试直接的json字符串, 不按字串中的顺序输出排列.
 json_str = '{"A":1, "B":2, "C":3, "D":4}'
 result = json.decode(json_str)
 
